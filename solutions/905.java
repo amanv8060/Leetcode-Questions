2bf6149196cbe378f2f7376ad3e26f3b0df2b99f
smondal1174 <subhadeep.mondal2001@gmail.com>
2021-10-01 08:47:27 +0530

    Create 905.java

---
+++ solutions/905.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j++]=temp;
+            }
+        }
+        return nums;
+    }
+}
