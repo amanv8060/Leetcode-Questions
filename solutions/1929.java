e0da744e3c3615a82afed98bce36ee70b3a5f493
subhendu-15 <91732457+subhendu-15@users.noreply.github.com>
2021-10-01 19:05:27 +0530

    Create 1929.java

---
+++ solutions/1929.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+       int n=nums.length;
+        int count=0;
+        int answer[] = new int[2*n];
+        for(int i=0;i<nums.length;i++)
+        {
+            answer[i]=answer[i+nums.length]=nums[i];
+        }
+        
+        return answer;
+    }
+}
