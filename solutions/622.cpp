1b866607cc01a307efca17156e25d04615e9c463
AA <41603039+Riz-Ali1@users.noreply.github.com>
2021-10-01 20:19:30 +0530

    Formated

--- solutions/622.cpp
+++ solutions/622.cpp
@@ -2,51 +2,64 @@
 using namespace std;
 
 class MyCircularQueue {
-public:
-    int front = 0, rear = -1;
-    int size; int max_size;
-    vector <int> vec;
-    
-    MyCircularQueue(int k) {
-        front = 0;
-        rear = -1;
-        vec.resize(k);
-        size = 0;
-        max_size = k;
+ public:
+  int front = 0, rear = -1;
+  int size;
+  int max_size;
+  vector<int> vec;
+
+  MyCircularQueue(int k) {
+    front = 0;
+    rear = -1;
+    vec.resize(k);
+    size = 0;
+    max_size = k;
+  }
+
+  bool enQueue(int value) {
+    if (isFull() == true) {
+      return false;
     }
-    
-    bool enQueue(int value) {
-        if(isFull()==true){return false;}
-        rear = (rear+1)%max_size;
-        vec[rear] = value;
-        size++;
-        return true;
+    rear = (rear + 1) % max_size;
+    vec[rear] = value;
+    size++;
+    return true;
+  }
+
+  bool deQueue() {
+    if (isEmpty() == true) {
+      return false;
     }
-    
-    bool deQueue() {
-        if(isEmpty()==true){return false;}
-        front = (front+1)%max_size;
-        size--;
-        return true;
+    front = (front + 1) % max_size;
+    size--;
+    return true;
+  }
+
+  int Front() {
+    if (isEmpty() == true) {
+      return -1;
     }
-    
-    int Front() {
-        if(isEmpty()==true){return -1;}
-        return vec[front];
+    return vec[front];
+  }
+
+  int Rear() {
+    if (isEmpty() == true) {
+      return -1;
     }
-    
-    int Rear() {
-        if(isEmpty()==true){return -1;}
-        return vec[rear];
+    return vec[rear];
+  }
+
+  bool isEmpty() {
+    if (size == 0) {
+      return true;
     }
-    
-    bool isEmpty() {
-        if(size==0){return true;}
-        return false;
+    return false;
+  }
+
+  bool isFull() {
+    if (size == max_size) {
+      return true;
     }
-    
-    bool isFull() {
-        if(size==max_size){return true;}
-        return false;
-    }
+    return false;
+  }
 };
