d5964f7eb0d1b0d91de591c56e3cef88b1bc43bb
Uğur Özcan <ugurozcan108@gmail.com>
2021-10-01 21:44:51 +0300

    Solution 14.py

---
+++ solutions/14.py
@@ -0,0 +1,10 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        shortest = min(strs,key=len)
+        for i, ch in enumerate(shortest):
+            for other in strs:
+                if other[i] != ch:
+                    return shortest[:i]
+        return shortest 
