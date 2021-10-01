fa70c29ce676ac53e846bca88f8edef899f9ac19
AA <41603039+Riz-Ali1@users.noreply.github.com>
2021-10-01 23:11:09 +0530

    Again Formatted

--- solutions/921.cpp
+++ solutions/921.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 class Solution {
  public:
   int minAddToMakeValid(string s) {
@@ -11,8 +10,7 @@ class Solution {
       if (s[i] == '(') {
         sta.push(s[i]);
         open++;
-      }
-      else if (sta.empty() == false and s[i] == ')' and sta.top() == '(') {
+      } else if (sta.empty() == false and s[i] == ')' and sta.top() == '(') {
         open--;
         sta.pop();
       } else {
