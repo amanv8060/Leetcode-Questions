d882eb9767d8011c69bce973dd9c8c2cd04d2148
AA <41603039+Riz-Ali1@users.noreply.github.com>
2021-10-01 20:11:18 +0530

    921 soloution

---
+++ solutions/921.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+    int open = 0;
+    int close = 0;
+    stack <char> sta;
+    for(int i=0; i<s.size(); i++)
+    {
+        if(s[i]=='(')
+        {
+            sta.push(s[i]);
+            open++;
+        }
+        //s = ")))(("  ()
+        else if(sta.empty()==false and s[i]==')' and sta.top()=='(')
+        {
+            open--;
+            sta.pop();
+        }
+        else
+        {
+            sta.push(s[i]);
+            close++;
+        }
+    }
+    return close+open;
+    }
+};
