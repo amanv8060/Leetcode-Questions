95e4a135a695d1d36be06c1e2f424dae6a695b8e
subhendu-15 <91732457+subhendu-15@users.noreply.github.com>
2021-10-01 19:10:44 +0530

    Create 1920.java

---
+++ solutions/1920.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int ans[] = new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            ans[i]=nums[nums[i]];            
+        }
+        return ans;
+    }
+}
