ea5dff856dc599a63d4b0172baa56a214d0b939c
AA <41603039+Riz-Ali1@users.noreply.github.com>
2021-10-01 20:23:04 +0530

    Formatted

--- solutions/108.cpp
+++ solutions/108.cpp
@@ -2,18 +2,19 @@
 using namespace std;
 
 class Solution {
-public:
-    TreeNode* constructTree(vector<int> nums, int start, int end)
-    {
-        if(start>end){return NULL;}
-        int mid = (start+end)/2;
-        TreeNode* root = new TreeNode(nums[mid]);
-        root->left = constructTree(nums, start, mid-1);
-        root->right = constructTree(nums, mid+1, end);
-        return root;
+ public:
+  TreeNode* constructTree(vector<int> nums, int start, int end) {
+    if (start > end) {
+      return NULL;
     }
-    TreeNode* sortedArrayToBST(vector<int>& nums) {
-        TreeNode* root = constructTree(nums, 0, nums.size()-1);
-        return root;
-    }
+    int mid = (start + end) / 2;
+    TreeNode* root = new TreeNode(nums[mid]);
+    root->left = constructTree(nums, start, mid - 1);
+    root->right = constructTree(nums, mid + 1, end);
+    return root;
+  }
+  TreeNode* sortedArrayToBST(vector<int>& nums) {
+    TreeNode* root = constructTree(nums, 0, nums.size() - 1);
+    return root;
+  }
 };
