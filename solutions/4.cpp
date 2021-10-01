0d07d38379bad0ea1dab85c02cd11cbed26a248c
classerase <60030956+classerase@users.noreply.github.com>
2021-10-01 16:52:40 +0100

    Create 4.cpp

---
+++ solutions/4.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size() + nums2.size();
+        
+        nums1.insert(nums1.end(), nums2.begin(), nums2.end());
+        
+        sort(nums1.begin(), nums1.end());
+        
+        if (n % 2 == 0) {
+            return (nums1[(double)n/2-1] + nums1[(double)n/2])/2.0;
+        } else {
+            return nums1[(double)n/2];
+        }
+    }
+};
