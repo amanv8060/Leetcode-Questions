dc8ca571222e84853c8ffc970ffa3fb333f1bb21
classerase <60030956+classerase@users.noreply.github.com>
2021-10-01 21:45:12 +0100

    Update 1833.cpp

--- solutions/1833.cpp
+++ solutions/1833.cpp
@@ -8,13 +8,13 @@ class Solution {
 
     int able_to_buy = 0;
 
-    for (int i = 0; i <= costs.size(); i++) {
+    for (int i = 0; i < costs.size(); i++) {
       if (coins - costs[i] >= 0) {
         able_to_buy++;
         coins -= costs[i];
       }
     }
 
-    return --able_to_buy;
+    return able_to_buy;
   }
 };
