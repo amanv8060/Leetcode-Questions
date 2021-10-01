8a889d070cc3bb241fb85c400767e34fa99e49f5
AA <41603039+Riz-Ali1@users.noreply.github.com>
2021-10-01 20:20:50 +0530

    Formatted code

--- solutions/921.cpp
+++ solutions/921.cpp
@@ -2,30 +2,24 @@
 using namespace std;
 
 class Solution {
-public:
-    int minAddToMakeValid(string s) {
+ public:
+  int minAddToMakeValid(string s) {
     int open = 0;
     int close = 0;
-    stack <char> sta;
-    for(int i=0; i<s.size(); i++)
-    {
-        if(s[i]=='(')
-        {
-            sta.push(s[i]);
-            open++;
-        }
-        //s = ")))(("  ()
-        else if(sta.empty()==false and s[i]==')' and sta.top()=='(')
-        {
-            open--;
-            sta.pop();
-        }
-        else
-        {
-            sta.push(s[i]);
-            close++;
-        }
+    stack<char> sta;
+    for (int i = 0; i < s.size(); i++) {
+      if (s[i] == '(') {
+        sta.push(s[i]);
+        open++;
+      }
+      else if (sta.empty() == false and s[i] == ')' and sta.top() == '(') {
+        open--;
+        sta.pop();
+      } else {
+        sta.push(s[i]);
+        close++;
+      }
     }
-    return close+open;
-    }
+    return close + open;
+  }
 };
