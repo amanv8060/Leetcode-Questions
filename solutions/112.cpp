2279f81c325a35e7068c1bb4ac5018a746dcf5ea
AA <41603039+Riz-Ali1@users.noreply.github.com>
2021-10-01 20:21:30 +0530

    Code formatted

--- solutions/112.cpp
+++ solutions/112.cpp
@@ -2,10 +2,15 @@
 using namespace std;
 
 class Solution {
-public:
-    bool hasPathSum(TreeNode* root, int targetSum) {
-        if(root == NULL){return false;}
-        if(root->val == targetSum and root->right == NULL and root->left == NULL){return true;}
-        return hasPathSum(root->left, targetSum-root->val) or hasPathSum(root->right, targetSum-root->val);
+ public:
+  bool hasPathSum(TreeNode* root, int targetSum) {
+    if (root == NULL) {
+      return false;
     }
+    if (root->val == targetSum and root->right == NULL and root->left == NULL) {
+      return true;
+    }
+    return hasPathSum(root->left, targetSum - root->val) or
+           hasPathSum(root->right, targetSum - root->val);
+  }
 };
