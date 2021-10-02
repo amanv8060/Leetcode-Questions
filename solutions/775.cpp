5ddeeb78130731f13e0deb3601581265142b05a7
Sourav Pramanik <70817147+sourav2b@users.noreply.github.com>
2021-10-02 19:31:28 +0530

    Update 775.cpp
    Formatted with Google Style Formatting

--- solutions/775.cpp
+++ solutions/775.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 class Solution {
-public:
+ public:
   bool isIdealPermutation(vector<int> &nums) {
     for (int i = 0; i < nums.size(); i++) {
-      if (abs(nums[i] - i) > 1)
-        return false;
+      if (abs(nums[i] - i) > 1) return false;
     }
     return true;
   }
