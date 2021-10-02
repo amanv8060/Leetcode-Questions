8ef45fd8bd06a940f28bb46cbec9a3a3c8bb6c4f
Sourav Pramanik <70817147+sourav2b@users.noreply.github.com>
2021-10-02 13:02:02 +0530

    Create 775.cpp

---
+++ solutions/775.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+  bool isIdealPermutation(vector<int> &nums) {
+    for (int i = 0; i < nums.size(); i++) {
+      if (abs(nums[i] - i) > 1)
+        return false;
+    }
+    return true;
+  }
+};
