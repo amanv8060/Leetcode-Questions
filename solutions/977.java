d5f05bf3ba8e64fb3ad85d590d901dce6e04d616
bubuL1234 <91711847+bubuL1234@users.noreply.github.com>
2021-10-02 09:42:22 +0530

    Create 977.java

---
+++ solutions/977.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int[] arr=new int[nums.length];
+        int i=0;
+        int j=nums.length-1;
+        int b=nums.length-1;
+        while(i<=j){
+            if(i==j){ arr[b--]=nums[i]*nums[i]; break;}
+            if(Math.abs(nums[i])>Math.abs(nums[j])){
+                arr[b--]=nums[i]*nums[i];
+                i++;
+            }
+            else if(Math.abs(nums[i])<Math.abs(nums[j])){
+                arr[b--]=nums[j]*nums[j];
+                j--;
+            }
+            else if(Math.abs(nums[i])==Math.abs(nums[j])){
+                arr[b--]=nums[i]*nums[i];
+                arr[b--]=nums[i]*nums[i];
+                i++;
+                j--;
+            }
+        }
+        return arr;
+    }
+}
