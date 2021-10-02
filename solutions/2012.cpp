01d64e3f4678a54cf5a97a0c5eea1143b9fec318
Moulik Agrawal <almoulik@gmail.com>
2021-10-02 18:50:16 +0530

    Formatting Changes

--- solutions/2012.cpp
+++ solutions/2012.cpp
@@ -1,35 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 class Solution {
-public:
-    int sumOfBeauties(vector<int>& nums) {
-         if(nums.size()==0)
-            return 0;
-    
-     
-        int sum=0;
-        
-        int left=nums[0];
-        vector<int>temp=nums;
-        sort(temp.begin()+2,temp.end());
-        int right=temp[2];
+ public:
+  int sumOfBeauties(vector<int>& nums) {
+    if (nums.size() == 0) return 0;
 
-        for(int i=1;i<=nums.size()-2;i++){
-            int maxim=0;
-            
-            if(nums[i-1]>left)
-                left=nums[i-1];
-        
-                right=temp[i+1];
-            
-            if(nums[i]>left && nums[i]<right)
-               maxim=max(maxim,2);
-            else
-            if(nums[i]>nums[i-1] && nums[i]<nums[i+1])
-                maxim=max(maxim,1);
-            sum+=maxim;
-        }
-        return sum;
+    int sum = 0;
+
+    int left = nums[0];
+    vector<int> temp = nums;
+    sort(temp.begin() + 2, temp.end());
+    int right = temp[2];
+
+    for (int i = 1; i <= nums.size() - 2; i++) {
+      int maxim = 0;
+
+      if (nums[i - 1] > left) left = nums[i - 1];
+
+      right = temp[i + 1];
+
+      if (nums[i] > left && nums[i] < right)
+        maxim = max(maxim, 2);
+      else if (nums[i] > nums[i - 1] && nums[i] < nums[i + 1])
+        maxim = max(maxim, 1);
+      sum += maxim;
     }
+    return sum;
+  }
 };
