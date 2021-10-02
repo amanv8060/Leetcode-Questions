457122844a6fb3e760f56f26b30ab0687a6bc5a4
Anusrita <69597798+Anusrita02@users.noreply.github.com>
2021-10-02 17:47:06 +0530

    create 1448.java (#1783)

---
+++ solutions/1448.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    int count=0;
+    public void helper(TreeNode root, int max){
+        if(root==null){
+            return;
+        }
+        if(root.val>=max){
+            max=root.val;
+            count+=1;
+            
+        }
+        
+        helper(root.left, max);
+        helper(root.right, max);
+        
+    }
+    public int goodNodes(TreeNode root) {
+        helper(root , root.val);
+        return count;
+    }
+}
