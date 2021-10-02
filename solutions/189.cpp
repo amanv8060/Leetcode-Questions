2aec82d317640dc530107c9f895c99f47be5beee
Shivangi-Mohanty <shivangimohanty2001@gmail.com>
2021-10-02 22:54:30 +0530

    solution_189 (#1813)
    * Create 189.cpp
    
    * Update 189.cpp
    
    Formatted using google format

---
+++ solutions/189.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+ public:
+  void rotate(vector<int> &nums, int k) {
+    int n = nums.size();
+    for (int i = 0; i < gcd(n, k); ++i)
+      for (int ii = (i + k) % n; i != ii; ii = (ii + k) % n)
+        swap(nums[i], nums[ii]);
+  }
+};
