6acbf1593986d605dc84b0c8576c3ef2a4a65d9a
Anh Hao Tran <anhhao2809@gmail.com>
2021-10-02 22:31:48 +0700

    update

--- solutions/88.java
+++ solutions/88.java
@@ -2,18 +2,11 @@ class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = m - 1;
         int j = n - 1;
+        int lastIndex = n + m - 1;
         if (n == 0) {
             return;
         }
-        if (m == 0) {
-            while(j >= 0) {
-                nums1[j] = nums2[j];
-                j--;
-            }
-            return;
-        }
         // start from last element
-        int lastIndex = n + m - 1;
         while (j >= 0)
         {
             if (i >= 0 && nums1[i] > nums2[j]) {
