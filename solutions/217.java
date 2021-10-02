db5df7c323a7db40608f27f28bd7304f0e14000b
Anh Hao Tran <anhhao2809@gmail.com>
2021-10-02 19:06:15 +0700

    solution 217

---
+++ solutions/217.java
@@ -0,0 +1,15 @@
+class Solution {
+  public boolean containsDuplicate(int[] nums) {
+    HashMap<Integer, Integer> hm = new HashMap<>();
+    boolean isDuplicated = false;
+    for(int i=0; i<nums.length; i++){
+        if(hm.containsKey(nums[i])){
+            isDuplicated = true;
+            break;
+        } else {
+            hm.put(nums[i], 1);
+        }
+    }
+    return isDuplicated;
+  }
+}
