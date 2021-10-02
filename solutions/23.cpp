d0aca7cf0aa1bed58ba906e00ebf150ef66ada2a
shubhankit123 <86077832+shubhankit123@users.noreply.github.com>
2021-10-02 10:44:40 +0530

    Update 23.cpp

--- solutions/23.cpp
+++ solutions/23.cpp
@@ -3,9 +3,19 @@ using namespace std
 
 //Method 1
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* merge(ListNode* a, ListNode* b){
+    ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
         if(a == nullptr)
             return b;
         if(b == nullptr)
@@ -13,52 +23,12 @@ public:
         ListNode* c;
         if(a->val < b->val){
             c = a;
-            c->next = merge(a->next, b);
+            c->next = mergeTwoLists(a->next, b);
         }
         else{
             c = b;
-            c->next = merge(a,b->next);
+            c->next = mergeTwoLists(a,b->next);
         }
         return c;
-    }
-    
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.size() == 0)
-            return nullptr;
-        
-        for(int i=1;i<lists.size();i++){
-            lists[0] = merge(lists[0],lists[i]);
-        }
-        return lists[0];
-    }
-};
-//Method 2
-
-class Solution {
-public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<int, vector<int>, greater<int>> pq;
-        for(int i=0;i<lists.size();i++){
-            while(lists[i]){
-                pq.push(lists[i]->val);
-                lists[i] = lists[i]->next;
-            }
-        }
-        ListNode* head = nullptr;
-        ListNode* curr = nullptr;
-        
-        while(!pq.empty()){
-            ListNode* temp = new ListNode(pq.top());
-            if(!curr){
-                curr = temp;
-                head = curr;
-            }
-            else{
-                curr->next = temp;
-                curr = curr->next;
-            }
-            pq.pop();
-        }
-        return head;
     }
 };
