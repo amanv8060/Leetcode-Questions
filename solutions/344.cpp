85d392f398449316d463109dfea3af2c2db39414
Shivangi-Mohanty <shivangimohanty2001@gmail.com>
2021-10-02 12:33:32 +0530

    Create 344.cpp

---
+++ solutions/344.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  void reverseString(vector<char> &s) {
+
+    int i;
+    int n = s.size();
+    for (i = 0; i < n / 2; i++) {
+      int temp = s[i];
+      s[i] = s[n - i - 1];
+      s[n - i - 1] = temp;
+    }
+  }
+};
