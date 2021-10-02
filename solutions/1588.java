2ff622904cfdd65b9c807bc8c1cb895bdcc4dba3
bubuL1234 <91711847+bubuL1234@users.noreply.github.com>
2021-10-02 18:10:11 +0530

    Create 1588.java (#1793)

---
+++ solutions/1588.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+        int sum=0;int res=0;
+        for(int i=0;i<arr.length;i++){
+            sum=0;
+            for(int j=i;j<arr.length;j++){
+                sum+=arr[j];
+                if((j-i+1)%2!=0 ){
+                    res+=sum;
+                }
+            }
+        }
+        
+        return res;
+    }
+}
