11f26044aa0754fcdf746006a54b9547bfeae95b
Shivangi-Mohanty <shivangimohanty2001@gmail.com>
2021-10-02 14:09:09 +0530

    Update 29.cpp

--- solutions/29.cpp
+++ solutions/29.cpp
@@ -1,15 +1,11 @@
 #include <bits/stdc++.h>
-using namespace std
+using namespace std;
 
-    int
-    divide(int dividend, int divisor) {
-  long int x, di, div;
-
-  if (dividend == INT_MIN && divisor == -1)
-    return INT_MAX;
-
-  else {
-    x = dividend / divisor;
-    return x;
+ class Solution {
+public:
+  int divide(int dividend, int divisor) {
+    if (dividend == INT_MIN && divisor == -1)
+      return INT_MAX;
+    return dividend / divisor;
   }
-}
+};
