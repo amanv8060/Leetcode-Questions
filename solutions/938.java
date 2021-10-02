9e5777b09c89e7d209e27db00d5aa870ea305cb7
bubuL1234 <91711847+bubuL1234@users.noreply.github.com>
2021-10-02 18:16:30 +0530

    Create 938.java (#1796)

---
+++ solutions/938.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    int sum=0;
+    public void helper(TreeNode root, int low, int high){
+        if(root==null){
+            return;
+        }
+        if(root.val>low){
+            helper(root.left, low, high);
+        }
+        if(root.val>=low && root.val<=high){
+            sum+=root.val;
+        }
+        if(root.val<high){
+            helper(root.right, low, high);
+        }
+    }
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        helper(root, low, high);
+        return sum;
+    }
+}
