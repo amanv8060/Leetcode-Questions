bb881af70c8720c4de024d3c2211af71e797efc5
Chinmay <20cs01011@iitbbs.ac.in>
2021-10-03 01:16:12 +0530

    added 20.cpp for leetcode question 20'

---
+++ solutions/20.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+      stack <char> st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('||s[i]=='['||s[i]=='{')
+                st.push(s[i]);
+            else if(s[i]==')')
+            {
+                if(!st.empty() && st.top()=='(')
+                    st.pop();
+                else
+                    return false; 
+                    
+            }
+            else if(s[i]==']')
+            {
+                if(!st.empty() && st.top()=='[')
+                    st.pop();
+                else
+                    return false; 
+            }
+            else if(s[i]=='}')
+            {
+                if(!st.empty() && st.top()=='{')
+                    st.pop();
+                else
+                    return false; 
+            }
+            else
+                return false;
+        }
+        if(!st.empty())
+            return false;
+        return true;
+    } 
+};
