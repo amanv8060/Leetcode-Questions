3d87507a722f1c70753ffb4af5b96d5b07d86610
Sourav Pramanik <70817147+sourav2b@users.noreply.github.com>
2021-10-02 19:55:49 +0530

    Update 121.cpp
    Formatted with Google Style Formatting

--- solutions/121.cpp
+++ solutions/121.cpp
@@ -1,15 +1,14 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int profit=0, price=INT_MAX;
-        for(int i=0;i<prices.size();i++)
-        {
-            price=min(price,prices[i]);
-            int present_profit=prices[i]-price;
-            profit=max(present_profit,profit);
-        }
-        return profit;
+ public:
+  int maxProfit(vector<int>& prices) {
+    int profit = 0, price = INT_MAX;
+    for (int i = 0; i < prices.size(); i++) {
+      price = min(price, prices[i]);
+      int present_profit = prices[i] - price;
+      profit = max(present_profit, profit);
     }
+    return profit;
+  }
 };
