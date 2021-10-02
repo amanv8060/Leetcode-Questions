9e0c691ff7c4c77594c0166db0f821e25f5133aa
Anh Hao Tran <anhhao2809@gmail.com>
2021-10-02 12:02:46 +0700

    solution 70

---
+++ solutions/70.java
@@ -0,0 +1,11 @@
+class Solution {
+  public int climbStairs(int n) {
+    int[] steps = new int[n + 1];
+    steps[0] = 1;
+    steps[1] = 1;
+    steps[2] = 2;
+    for (int i = 3; i <= n; i++)
+      steps[i] = steps[i - 1] + steps[i - 2];
+    return steps[n];
+  }
+}
