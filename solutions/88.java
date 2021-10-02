a29a8fd3e27feb0f8f049fea5654848a64a39210
Anh Hao Tran <anhhao2809@gmail.com>
2021-10-02 22:10:45 +0700

    update solution

--- solutions/88.java
+++ solutions/88.java
@@ -1,24 +1,26 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = n - 1;
-        int j = m - 1;
-        int lastIndex = n + m - 1;
-         
-        // start from last element in each array
-        while (j >= 0)
-        {
-            /* End of nums1 is greater than end of nums2 */
-            if (i >= 0 && nums1[i] > nums2[j])
-            {
-                nums1[lastIndex] = nums1[i];
-                i--;
-            } else
-            { 
-                nums1[lastIndex] = nums2[j];
+        int i = m - 1;
+        int j = n - 1;
+        if (n == 0) {
+            return;
+        }
+        if (m == 0) {
+            while(j >= 0) {
+                nums1[j] = nums2[j];
                 j--;
             }
-            // Move indices
-            lastIndex--;
+            return;
+        }
+        // start from last element
+        int lastIndex = n + m - 1;
+        while (j >= 0)
+        {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[lastIndex--] = nums1[i--];
+            } else { 
+                nums1[lastIndex--] = nums2[j--];
+            }
         }
     }
 }
