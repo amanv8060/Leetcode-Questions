cebcb5bc92538d24d9c1b873d615b8caecd483de
Shivangi-Mohanty <shivangimohanty2001@gmail.com>
2021-10-03 01:48:01 +0530

    solution_1942 (#1804)
    * Create 1942.cpp
    
    * Update 1942.cpp
    
    Formatted using google formatting

---
+++ solutions/1942.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+ public:
+  int smallestChair(vector<vector<int>> &times, int targetFriend) {
+    int time = times[targetFriend][0];
+    vector<int> arrival(time + 1, -1);
+
+    for (int i = 0; i < times.size(); i++) {
+      if (times[i][0] <= time) arrival[times[i][0]] = i;
+    }
+    int chair = -1;
+    for (int i = 1; i <= time; i++) {
+      if (arrival[i] != -1) {
+        chair++;
+        int j = i;
+        while (j < time) {
+          int next = arrival[j];
+          if (next != -1) {
+            arrival[j] = -1;
+            j = times[next][1];
+          } else
+            j++;
+        }
+        if (j == time) break;
+      }
+    }
+    return chair;
+  }
+};
