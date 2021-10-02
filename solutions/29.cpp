6bc2331a18c951bbcc8587004b8dfa1ab5c2c91e
Shivangi-Mohanty <shivangimohanty2001@gmail.com>
2021-10-02 22:57:24 +0530

    solution_29 (#1800)
    * Create 29.c
    
    * Create 29.cpp
    
    * Delete 29.c
    
    * Update 29.cpp
    
    * Update 29.cpp
    
    Formatted with google style formatting

---
+++ solutions/29.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+ public:
+  int divide(int dividend, int divisor) {
+    if (dividend == INT_MIN && divisor == -1) return INT_MAX;
+    return dividend / divisor;
+  }
+};
