6d9f372ebf7547fff80e7a5351f2d80a50c2b851
Chinmay <20cs01011@iitbbs.ac.in>
2021-10-03 17:32:19 +0530

    Adding solution for 20

--- solutions/20.cpp
+++ solutions/20.cpp
@@ -2,39 +2,32 @@
 using namespace std;
 
 class Solution {
-public:
-    bool isValid(string s) {
-      stack <char> st;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='('||s[i]=='['||s[i]=='{')
-                st.push(s[i]);
-            else if(s[i]==')')
-            {
-                if(!st.empty() && st.top()=='(')
-                    st.pop();
-                else
-                    return false; 
-                    
-            }
-            else if(s[i]==']')
-            {
-                if(!st.empty() && st.top()=='[')
-                    st.pop();
-                else
-                    return false; 
-            }
-            else if(s[i]=='}')
-            {
-                if(!st.empty() && st.top()=='{')
-                    st.pop();
-                else
-                    return false; 
-            }
-            else
-                return false;
-        }
-        if(!st.empty())
-            return false;
-        return true;
-    } 
+ public:
+  bool isValid(string s) {
+    stack<char> st;
+    for (int i = 0; i < s.size(); i++) {
+      if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+        st.push(s[i]);
+      else if (s[i] == ')') {
+        if (!st.empty() && st.top() == '(')
+          st.pop();
+        else
+          return false;
+
+      } else if (s[i] == ']') {
+        if (!st.empty() && st.top() == '[')
+          st.pop();
+        else
+          return false;
+      } else if (s[i] == '}') {
+        if (!st.empty() && st.top() == '{')
+          st.pop();
+        else
+          return false;
+      } else
+        return false;
+    }
+    if (!st.empty()) return false;
+    return true;
+  }
 };
