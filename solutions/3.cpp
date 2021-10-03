ad4e95648e1cd97854edfda2efbff7156c313a8e
Khushi-agarwal <khushiagarwal226@gmail.com>
2021-10-03 13:47:39 +0530

    Syntax corrected

--- solutions/3.cpp
+++ solutions/3.cpp
@@ -2,25 +2,27 @@
 using namespace std;
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-         int window_len=0;
-        int max_window_len=0;
-        int i=0; int j=0;
-        unordered_map<char,int> m;
-        while(j<s.length()){
-            char ch=s[j];
-                i=m[ch]+1;
-                //update window length
-                window_len=j-i;
-            }
-            //if new unique character then push character and its occurance in the map
-            m[ch]=j;
-            window_len++;
-            j++;
-            //update longest unique substring length
-            max_window_len=max(max_window_len,window_len);
+    int lengthOfLongestSubstring(string s)
+    {
+        int window_len = 0;
+        int max_window_len = 0;
+        int i = 0;
+        int j = 0;
+        unordered_map<char, int> m;
+        while (j < s.length()) {
+            char ch = s[j];
+            i = m[ch] + 1;
+            //update window length
+            window_len = j - i;
         }
-        return max_window_len;
-        
+        //if new unique character then push character and its occurance in the map
+        m[ch] = j;
+        window_len++;
+        j++;
+        //update longest unique substring length
+        max_window_len = max(max_window_len, window_len);
     }
-};
+    return max_window_len;
+}
+}
+;
