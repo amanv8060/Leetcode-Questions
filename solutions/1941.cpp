3cc6b71528984106286039a5f617705e1d4a1017
Hitesh <76858968+hitesh1906@users.noreply.github.com>
2021-10-03 22:03:05 +0530

    Update 1941.cpp

--- solutions/1941.cpp
+++ solutions/1941.cpp
@@ -3,21 +3,20 @@ using namespace std;
 
 class Solution {
 public:
-    bool areOccurrencesEqual(string s) {
+    bool areOccurrencesEqual(string s)
+    {
         int character;
-        int arr[26]={ };
-        for(int i=0;i<s.length();i++)
-        {
-            character=s[i];
-            arr[character-97]++;
+        int arr[26] = {};
+        for (int i = 0; i < s.length(); i++) {
+            character = s[i];
+            arr[character - 97]++;
         }
-        
-        for(int i=0;i<26;i++)
-        {
-           if(arr[i]==*max_element(arr, arr + 26) || arr[i]==0)
-               continue;
-            
-           if(arr[i]<*max_element(arr, arr + 26)|| arr[i]>*max_element(arr, arr + 26))
+
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] == *max_element(arr, arr + 26) || arr[i] == 0)
+                continue;
+
+            if (arr[i] < *max_element(arr, arr + 26) || arr[i] > *max_element(arr, arr + 26))
                 return false;
         }
         return true;
