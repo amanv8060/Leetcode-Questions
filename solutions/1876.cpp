682ebb6521cea8a9a729909ddfc418a42f614134
Hitesh <76858968+hitesh1906@users.noreply.github.com>
2021-10-03 22:13:51 +0530

    Update 1876.cpp

--- solutions/1876.cpp
+++ solutions/1876.cpp
@@ -3,37 +3,32 @@ using namespace std;
 
 class Solution {
 public:
-   int countGoodSubstrings(string s) {
-        int count=0;
-        vector<char>v;
-       
-       if(s.length()<3)
-       {
-           return 0;
-       }
-       else
-       {
-            for(int i=0;i<s.length()-2;i++)
-        {
-            for(int j=i;j<i+3;j++)
-            {
-                v.push_back(s[j]);
-            }
+    int countGoodSubstrings(string s)
+    {
+        int count = 0;
+        vector<char> v;
 
-            sort(v.begin(),v.end());
-
-            auto ip = unique(v.begin(), v.begin()+v.size());
-
-            v.resize(distance(v.begin(),ip));
-
-            if(v.size()==3)
-            {
-                count++;
-            }
-            v.clear();
+        if (s.length() < 3) {
+            return 0;
         }
-        return count;
-       }
-       
-   }
+        else {
+            for (int i = 0; i < s.length() - 2; i++) {
+                for (int j = i; j < i + 3; j++) {
+                    v.push_back(s[j]);
+                }
+
+                sort(v.begin(), v.end());
+
+                auto ip = unique(v.begin(), v.begin() + v.size());
+
+                v.resize(distance(v.begin(), ip));
+
+                if (v.size() == 3) {
+                    count++;
+                }
+                v.clear();
+            }
+            return count;
+        }
+    }
 };
