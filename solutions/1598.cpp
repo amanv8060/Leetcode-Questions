8442a90c3555ab44d2aa5f0a2f6bfd171a115cd0
Karan Deep <56315987+kar-cloud@users.noreply.github.com>
2021-10-03 17:14:14 +0530

    added 1598.cpp

---
+++ solutions/1598.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int result = 0;
+        for(int i=0; i<logs.size(); i++){
+            if(logs[i] == "../"){
+                if(result != 0){
+                    result--;
+                }
+            }
+            else if(logs[i] != "./"){
+                result++;
+            }
+        }
+        return result;
+    }
+};
