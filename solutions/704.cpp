c534ef9dd7fad57b05dfd000382b2e385f44c1c0
Md Atif <64218887+Atif0604@users.noreply.github.com>
2021-10-06 15:27:13 +0530

    Update 704.cpp

--- solutions/704.cpp
+++ solutions/704.cpp
@@ -5,12 +5,12 @@ class Solution {
  public:
   int search(vector<int>& nums, int target) {
     int s = 0;
-    int e = arr.size() - 1;
+    int e = nums.size() - 1;
     while (s <= e) {
       int mid = (s + e) / 2;
-      if (arr[mid] == target)
+      if (nums[mid] == target)
         return mid;
-      else if (arr[mid] < target)
+      else if (nums[mid] < target)
         s = mid + 1;
       else
         e = mid - 1;
